d331276f71196276edb7a39743d78a3d408bd681
chrisf <chrisf@14f77d18-25ff-4ae7-ba30-1ef6fd2e1af3>
2009-04-12 04:16:52 +0000

    guard in own file; use right coord system for guard planning, so the pathing actually works.
    more complex guard behavior required.
    
    git-svn-id: svn://svn.ijw.co.nz/svn2/AlienGame@75 14f77d18-25ff-4ae7-ba30-1ef6fd2e1af3

---
+++ Actors/Guard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Xml;
+
+namespace AlienGame.Actors
+{
+	class Guard : Mover
+	{
+		public override void Draw(Graphics g)
+		{
+			g.DrawRectangle(Pens.Fuchsia,
+					Position.X - 10, Position.Y - 10, 20, 20);
+			g.DrawString("Guard\n" + Name, Form1.font, Brushes.White, Position.X - 8, Position.Y - 8);
+		}
+
+		public Guard() : base() { }
+		public Guard(XmlElement e) : base(e) { }
+
+		public override void Use(Model m, Actor user)
+		{
+			// we've just got a tip about an alien at `user`.
+			SetOrders(PlanPathTo(m, user.Position.ToSquare()));
+
+			// todo: use a different planner, or override it for spotting an alien!
+			// todo: mill around after reaching target position
+			// todo: jitter target position so we dont look dumb when there's more than one dude spawned
+			// todo: get bored and go back to the spawner [and unspawn] after a while
+		}
+	}
+}
