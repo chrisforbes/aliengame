cdb432caa3e8f2cd3d51aeead2c80395fc901ee1
chrisf <chrisf@14f77d18-25ff-4ae7-ba30-1ef6fd2e1af3>
2009-04-18 09:58:43 +0000

    ray test looks like it actually works now.
    git-svn-id: svn://svn.ijw.co.nz/svn2/AlienGame@98 14f77d18-25ff-4ae7-ba30-1ef6fd2e1af3

--- Tools/RayTestTool.cs
+++ Tools/RayTestTool.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 using System.Windows.Forms;
+using IjwFramework.Types;
 
 namespace AlienGame.Tools
 {
@@ -48,6 +49,43 @@ namespace AlienGame.Tools
 
 			g.DrawRectangle(Pens.LimeGreen, start.Value.ToPointRect().SquaresToPixels());
 			g.DrawRectangle(Pens.LimeGreen, end.Value.ToPointRect().SquaresToPixels());
+
+			g.DrawLine(Pens.LimeGreen, start.Value.SquareToCenter(), end.Value.SquareToCenter());
+
+			foreach (var w in WallsUnderRay(start.Value.SquareToCenter(), end.Value.SquareToCenter(), m))
+				DrawCross(g,
+					Lerp(0.5f, w.First.SquareToCenter(), w.Second.SquareToCenter()));
+		}
+
+		static void DrawCross(Graphics g, Point p)
+		{
+			g.DrawLine(Pens.Red, p.X - 5, p.Y - 5, p.X + 5, p.Y + 5);
+			g.DrawLine(Pens.Red, p.X - 5, p.Y + 5, p.X + 5, p.Y - 5);
+		}
+
+		static IEnumerable<Pair<Point, Point>> WallsUnderRay(Point a, Point b, Model m)
+		{
+			var tiles = TilesUnderRay(a, b);
+			var prev = a.ToSquare();
+
+			foreach (var t in tiles)
+			{
+				var i = MakeDirIndex(t.X - prev.X, t.Y - prev.Y);
+				var mask = m.Cache.Value.GetWallMask(prev.X, prev.Y);
+				if (i != null && mask != null)
+					if ((mask.Value & (5 << i)) == (1 << i))
+						yield return new Pair<Point, Point>(t, prev);
+				prev = t;
+			}
+		}
+
+		static int? MakeDirIndex(int dx, int dy)
+		{
+			if (dx == 1 && dy == 0) return 0;
+			if (dx == 0 && dy == 1) return 1;
+			if (dx == -1 && dy == 0) return 4;
+			if (dx == 0 && dy == -1) return 5;
+			return null;
 		}
 
 		static IEnumerable<Point> TilesUnderRay(Point a, Point b)
@@ -65,12 +103,17 @@ namespace AlienGame.Tools
 			yield return p;
 			for (var f = 0.0f; f <= 1.0f; f += frac)
 			{
-				var q = new Point( 
-					(int)((1 - f) * a.X + f * b.X),
-					(int)((1 - f) * a.Y + f * b.Y)).ToSquare();
+				var q = Lerp(f,a,b).ToSquare();
 
 				if (p != q) yield return p = q;
 			}
+		}
+
+		static Point Lerp(float t, Point a, Point b)
+		{
+			return new Point(
+					(int)((1 - t) * a.X + t * b.X),
+					(int)((1 - t) * a.Y + t * b.Y));
 		}
 	}
 }
